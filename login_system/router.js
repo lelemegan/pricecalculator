d6583a2228da47b59bcd8de5e8154c430ce021dd
lelemegan <tiongmegan@gmail.com>
2022-01-29 14:39:42 +0800

    basic logout function done

--- login_system/router.js
+++ login_system/router.js
@@ -11,11 +11,32 @@ const credential = {
 router.post('/login',(req,res)=>{
     if(req.body.email == credential.email && req.body.password == credential.password){
         req.session.user = req.body.email;
-        //res.redirect('/dashboard');
-        res.end("Login Successful");
+        res.redirect('/route/dashboard');
+        //res.end("Login Successful");
     }else{
         res.end("Invalid Username")
     }
+})
+
+// route for dashboard
+router.get('/dashboard',(req,res) => {
+    if(req.session.user){
+        res.render('dashboard', {user:req.session.user})
+    }else{
+        res.send("Unauthorised User")
+    }
+})
+
+// route for logout
+router.get("/logout",(req, res)=>{
+    req.session.destroy(function(err){
+        if(err){
+        console.log(err);
+        res.send("Error")
+        }else{
+            res.render('base', {title:"Express", logout:"logout successfulllll woohoo"})
+        }
+    })
 })
 
 module.exports = router;
